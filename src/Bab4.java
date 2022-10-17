131cacb20e8c208a71db3cc546fbb19fdde511c7
Hapidr <hapidr24@gmail.com>
2022-10-17 17:44:43 +0700

    add new file

---
+++ src/Bab4.java
@@ -0,0 +1,16 @@
+import java.util.Scanner;
+
+public class Bab4 {
+    public static void main(String[] args) {
+        Scanner input = new Scanner(System.in);
+        double r, t, volume, phi;
+        phi = 3.14;
+        System.out.print("Masukkan jari-jari : ");
+        r = input.nextFloat();
+        System.out.print("Masukkan tinggi : ");
+        t = input.nextFloat();
+
+        volume = phi * (r*r) * t;
+        System.out.println("Volume tabung adalah = " + volume);
+    }
+}
---
+++ src/RataRata.java
@@ -0,0 +1,8 @@
+public class RataRata {
+    public static void main(String[] args) {
+        float a = 5, b = 9, c = 7;
+        double rata_rata = (a+b+c)/3;
+        System.out.println("Rata-rata adalah = " + rata_rata);
+
+    }
+}
