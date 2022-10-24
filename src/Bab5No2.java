534f281410128f1ca796389a1b0cc7565e6ed91e
Hapidr <hapidr24@gmail.com>
2022-10-24 09:35:53 +0700

    add new file Bab5No2.java

---
+++ src/Bab5No2.java
@@ -0,0 +1,17 @@
+import java.util.Scanner;
+
+public class Bab5No2 {
+    public static void main(String[] args) {
+        Scanner input = new Scanner(System.in);
+        System.out.print("Masukkan suhu: ");
+        int suhu = input.nextInt();
+
+        if (suhu > 100){
+            System.out.println("berwujud Uap");
+        } else if (suhu >= 0 && suhu <= 100){
+            System.out.println("Berwujud cair");
+        } else{
+            System.out.println("Berwujud Padat");
+        }
+    }
+}
