a244374f2ed4e99b060a11f2a4482e800491d92f
Hapidr <hapidr24@gmail.com>
2022-10-24 09:28:21 +0700

    add new file Bab5No3.java

---
+++ src/Bab5No3.java
@@ -0,0 +1,17 @@
+import java.util.Scanner;
+
+public class Bab5No3 {
+    public static void main(String[] args) {
+        Scanner input = new Scanner(System.in);
+        System.out.print("Masukkan angka: ");
+        int angka = input.nextInt();
+
+        if (angka == 0){
+            System.out.println(angka + " adalah NOL");
+        } else if(angka > 0){
+            System.out.println(angka + " adalah positif");
+        } else{
+            System.out.println(angka + " adalah negatif");
+        }
+    }
+}
