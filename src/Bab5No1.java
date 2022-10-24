d4b33a9bbdf6cea5f4e7ee82fefb2169b5fc1a2d
Hapidr <hapidr24@gmail.com>
2022-10-24 08:46:07 +0700

    add new file Bab5No1

---
+++ src/Bab5No1.java
@@ -0,0 +1,24 @@
+import java.util.Scanner;
+
+public class Bab5No1 {
+    public static void main(String[] args) {
+        Scanner input = new Scanner(System.in);
+        System.out.print("Masukkan nilai a: ");
+        int a = input.nextInt();
+        System.out.print("Masukkan nilai b: ");
+        int b = input.nextInt();
+        System.out.print("Masukkan nilai c: ");
+        int c = input.nextInt();
+
+        int d = b*b-(4*a*c);
+        if (d == 0){
+            System.out.println("Akar kembar");
+        } else if (d > 0){
+            System.out.println("Akar beda");
+        } else{
+            System.out.println("Akar imajiner");
+        }
+
+    }
+
+}
