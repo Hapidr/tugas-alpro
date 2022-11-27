25c4392bc9d8cf0ad55ebad6d78f17d775c05853
Hapidr <hapidr24@gmail.com>
2022-11-27 08:13:34 +0700

    initial commmit

---
+++ src/Bab7No2For.java
@@ -0,0 +1,11 @@
+
+public class Bab7No2For {
+    public static void main(String[] args) {
+        int end = 500;
+        for (int i=1;i<=end;i++){
+            if (i % 5 == 0){
+                System.out.println(i);
+            }
+        }
+    }
+}
