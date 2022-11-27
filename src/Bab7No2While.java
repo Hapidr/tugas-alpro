769538c25f5df5193da2e2f667dacd2b04d68d50
Hapidr <hapidr24@gmail.com>
2022-11-27 08:24:30 +0700

    initial commmit

---
+++ src/Bab7No2While.java
@@ -0,0 +1,11 @@
+public class Bab7No2While {
+    public static void main(String[] args) {
+        int i = 1;
+        while (i<=500){
+            if (i % 5 == 0){
+                System.out.println(i);
+            }
+            i++;
+        }
+    }
+}
