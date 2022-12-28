4815057f8d0daa49c3c104bce06fb6c64757c271
Hapidr <hapidr24@gmail.com>
2022-12-28 14:55:49 +0700

    add Larik1

---
+++ src/Larik1.java
@@ -0,0 +1,28 @@
+import java.util.Scanner;
+
+public class Larik1 {
+    public static void main(String[] args) {
+        Scanner input = new Scanner(System.in);
+        float[] nilai = new float[5];
+
+        System.out.println("Masukkan 5 buah data nilai ");
+
+        for (int i = 0; i < 5; i++) {
+            System.out.print("Data ke-" + (i + 1) + ": ");
+            nilai[i] = input.nextFloat();
+        }
+
+        System.out.println("Data nilai yang dimasukkan");
+        for (int i = 0; i < 5; i++) {
+            System.out.print(nilai[i] + " | ");
+        }
+
+        float jumlah = 0;
+        for (int i = 0; i < 5; i++) {
+            jumlah += nilai[i];
+        }
+
+        System.out.println("\nJumlah = " + jumlah);
+        System.out.println("Rata-rata = " + (jumlah/nilai.length));
+    }
+}
